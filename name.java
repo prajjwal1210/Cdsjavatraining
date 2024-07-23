99c22725a241c319b5601fc72791f68668fba999
Prajjwal Verma <144658943+prajjwal1210@users.noreply.github.com>
2024-07-23 21:45:54 +0530

    Name.java

--- name.java
+++ name.java
@@ -1,10 +1,3 @@
-
-/*
-Online Java - IDE, Code Editor, Compiler
-
-Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
-*/
-
 public class Main
 {
     public static void main(String[] args) {
